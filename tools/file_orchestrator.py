e1797dcee82ef80bf0970957ab42b9687f9b702c
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2023-12-07 00:30:15 +0100

    The provided commit message is incorrect for the given code diffs. The correct commit message should be:
    Add new FileOrchestrator tool
    
    This commit introduces the FileOrchestrator class in the tools directory. This new class provides functions for handling user pieces data, retrieving PDFs and converting the first page of PDFs into images.
    
    Signed-off-by: discovery-bit <61625792+discovery-bit@users.noreply.github.com>

---
+++ tools/file_orchestrator.py
@@ -0,0 +1,29 @@
+import tempfile
+
+from pdf2image import convert_from_path
+
+from . import models
+
+
+class FileOrchestrator:
+    def __init__(self):
+        ...
+
+    def get_user_pieces(self, user: models.User):
+        return [322, 111, 333]
+
+    def get_piece_pdf(self, piece_id) -> bytes:
+        models.getFilePath(piece_id=piece_id)
+        ...
+
+    def get_piece_image(self, piece_id: int):
+        with tempfile.TemporaryFile() as temp_pdf:
+            temp_pdf.write(self.get_piece_pdf(piece_id))
+
+            # Convert the first page of the pdf to an image
+            images = convert_from_path(
+                temp_pdf.name, dpi=300, poppler_path="path_if_needed"
+            )
+
+            # Assuming we just want the first image
+            return images[0] if images else None
