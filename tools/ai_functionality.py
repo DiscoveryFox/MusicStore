5817b29d80b403baa665c2928ca1fa74e2dc23d6
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2023-12-20 00:01:01 +0100

    Refactor AI functionality and support multiple models
    Existing OpenAI functionality has been refactored into standalone functions for better modularity and readability. The 'extract_instruments' function has been upgraded to support both "gpt-3.5-turbo" and "gpt-j" models. The specific model to use can be passed as an argument. This allows the system to handle more diverse AI Models for better song and instrument detection.
    
    Signed-off-by: discovery-bit <61625792+discovery-bit@users.noreply.github.com>

--- tools/ai_functionality.py
+++ tools/ai_functionality.py
@@ -1,6 +1,8 @@
 import os
+import typing
 
 import dotenv
+import requests
 from openai import OpenAI
 
 from . import pdf_extractor
@@ -11,22 +13,7 @@ dotenv.load_dotenv()
 def get_song_name(filename: str):
     content: str = pdf_extractor.extract_text_from_pdf(filename, ai_ready=True)
 
-    client = OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),
-    )
-
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "Please extract only the name of the song. Answer only with the title of the song.",
-            },
-            {"role": "user", "content": content},
-        ],
-    )
-
-    return completion.choices[0].message.content
+    return get_song_name_from_content(content)
 
 
 def get_song_name_from_content(content: str):
@@ -54,37 +41,48 @@ def extract_instruments_from_pdf_raw_response(filepath: str) -> str:
     return "Trumpet"
 
 
-def extract_instruments(filename: str) -> str:
+def extract_instruments(
+    filename: str, model: str = typing.Literal["gpt-3.5-turbo", "gpt-j"]
+) -> str:
     content: str = pdf_extractor.extract_text_from_pdf(filename, ai_ready=True)
 
-    client = OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),
-    )
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "Please extract the instrument based on this answer. Only return the name of the instrument. No more text. If there are more than one instrument mentioned return 'Multiple Instruments'",
-            },
-            {"role": "user", "content": content},
-        ],
-    )
-    return completion.choices[0].message.content
+    return extract_instruments_from_content(content, model)
 
 
-def extract_instruments_from_content(content: str) -> str:
-    client = OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),
-    )
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "Please extract the instrument based on this answer. Only return the name of the instrument. No more text. If there are more than one instrument mentioned return 'Multiple Instruments'",
-            },
-            {"role": "user", "content": content},
-        ],
-    )
-    return completion.choices[0].message.content
+def extract_instruments_from_content(
+    content: str, model: str = typing.Literal["gpt-3.5-turbo", "gpt-j"]
+) -> str:
+    match str(model).lower():
+        case "gpt-3.5-turbo":
+            client = OpenAI(
+                api_key=os.getenv("OPENAI_API_KEY"),
+            )
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Please extract the instrument based on this answer. Only return the name of the instrument. No more text. If there are more than one instrument mentioned return 'Multiple Instruments'",
+                    },
+                    {"role": "user", "content": content},
+                ],
+            )
+            return completion.choices[0].message.content
+        case "gpt-j":
+            file_content = content
+            context = f"Please extract the instrument based on this answer. Only return the name of the instrument. No more text. If there are more than one instrument mentioned return 'Multiple Instruments'. This is the file: {file_content}"
+            payload = {
+                "context": context,
+                "token_max_length": 4096,
+                "temperature": 1.0,
+                "top_p": 0.9,
+            }
+
+            print("Sending request")
+            response = requests.post(
+                "http://api.vicgalle.net:5000/docs#/default/generate_generate_post",
+                params=payload,
+            ).json()
+            print("Request send")
+            print(response)
+            return response
